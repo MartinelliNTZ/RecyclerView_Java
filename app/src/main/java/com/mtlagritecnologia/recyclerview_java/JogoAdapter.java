8396d8c776cbc492b81a4bc0fe6942ec0a5eb46b
MartinelliNTZ <martinelli.matheus0@gmail.com>
2025-07-06 13:53:43 -0300

    finalizado

--- app/src/main/java/com/mtlagritecnologia/recyclerview_java/JogoAdapter.java
+++ app/src/main/java/com/mtlagritecnologia/recyclerview_java/JogoAdapter.java
@@ -1,8 +1,14 @@
 package com.mtlagritecnologia.recyclerview_java;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * @author Matheus Martinelli
@@ -10,19 +16,36 @@ import android.view.ViewGroup;
  * martinelli.matheus2@gmail.com
  */
 public class JogoAdapter extends RecyclerView.Adapter<JogoViewHolder> {
+    private Context context;
+    private ArrayList<Jogo> itens;
+
+    public JogoAdapter(Context context, ArrayList<Jogo> itens) {
+        this.context = context;
+        this.itens = itens;
+    }
+
     @NonNull
     @Override
-    public JogoViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public JogoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int typeView) {
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.jogo_linha, parent, false);
+        JogoViewHolder jogoViewHolder = new JogoViewHolder(view);
+        return jogoViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull JogoViewHolder jogoViewHolder, int i) {
+    public void onBindViewHolder(@NonNull JogoViewHolder jogoViewHolder, int position) {
+        Jogo jogo = itens.get(position);
+        jogoViewHolder.txtNome.setText(jogo.getNome());
+        jogoViewHolder.txtPlataform.setText(jogo.getPlataform());
+        jogoViewHolder.txtNome.setOnClickListener(v ->{
+            Toast.makeText(context, "Clicou em "+"jogo.getNome()", Toast.LENGTH_SHORT).show();
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 100;
+        return itens.size();
     }
 }
--- app/src/main/java/com/mtlagritecnologia/recyclerview_java/MainActivity.java
+++ app/src/main/java/com/mtlagritecnologia/recyclerview_java/MainActivity.java
@@ -2,12 +2,63 @@ package com.mtlagritecnologia.recyclerview_java;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recycler;
+    private JogoAdapter adapter;
+    private ArrayList<Jogo> itens;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recycler = findViewById(R.id.recycler);
+        criarLista();
+        adapter = new JogoAdapter(MainActivity.this,itens);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this,
+                LinearLayoutManager.VERTICAL, false);
+        recycler.setLayoutManager(layoutManager);
+        recycler.setAdapter(adapter);
     }
+
+    public void criarLista() {
+        // Inicializa a lista
+        itens = new ArrayList<Jogo>();
+
+        // Adiciona vários jogos à lista
+        itens.add(new Jogo("God of War", "PlayStation"));
+        itens.add(new Jogo("Halo Infinite", "Xbox"));
+        itens.add(new Jogo("The Legend of Zelda: Breath of the Wild", "Nintendo Switch"));
+        itens.add(new Jogo("Counter-Strike: Global Offensive", "PC"));
+        itens.add(new Jogo("Minecraft", "Multiplataforma"));
+        itens.add(new Jogo("Red Dead Redemption 2", "PlayStation"));
+        itens.add(new Jogo("Forza Horizon 5", "Xbox"));
+        itens.add(new Jogo("Super Mario Odyssey", "Nintendo Switch"));
+        itens.add(new Jogo("The Witcher 3: Wild Hunt", "PC"));
+        itens.add(new Jogo("Elden Ring", "PlayStation"));
+        itens.add(new Jogo("Gears 5", "Xbox"));
+        itens.add(new Jogo("Animal Crossing: New Horizons", "Nintendo Switch"));
+        itens.add(new Jogo("Valorant", "PC"));
+        itens.add(new Jogo("Final Fantasy VII Remake", "PlayStation"));
+        itens.add(new Jogo("Sea of Thieves", "Xbox"));
+        itens.add(new Jogo("Splatoon 3", "Nintendo Switch"));
+        itens.add(new Jogo("League of Legends", "PC"));
+        itens.add(new Jogo("Horizon Forbidden West", "PlayStation"));
+        itens.add(new Jogo("Microsoft Flight Simulator", "Xbox"));
+        itens.add(new Jogo("Metroid Dread", "Nintendo Switch"));
+        itens.add(new Jogo("Fortnite", "Multiplataforma"));
+        itens.add(new Jogo("Call of Duty: Warzone", "Multiplataforma"));
+        itens.add(new Jogo("Resident Evil Village", "PlayStation"));
+        itens.add(new Jogo("FIFA 23", "Multiplataforma"));
+        itens.add(new Jogo("Cyberpunk 2077", "PC"));
+
+        // Você pode adicionar mais conforme desejar!
+    }
+
 }
